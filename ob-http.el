d9af67c6726c50447d8cf21b9c1c25a3a07dc0ce
Feng Zhou <zf.pascal@gmail.com>
2015-11-28 18:23:00 +0800

    GPL

--- ob-http.el
+++ ob-http.el
@@ -1,11 +1,24 @@
 ;;; ob-http.el --- http request in org-mode babel
 
-;; Copyright (C) 2015 ZHOU Feng
+;; Copyright (C) 2015 Feng Zhou
 
 ;; Author: ZHOU Feng <zf.pascal@gmail.com>
 ;; URL: http://github.com/zweifisch/ob-http
 ;; Version: 0.0.1
 ;; Package-Requires: ((s "1.9.0") (cl-lib "0.5"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
